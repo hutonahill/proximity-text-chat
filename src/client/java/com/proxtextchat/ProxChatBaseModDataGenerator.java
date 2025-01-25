5e19c4845f24cec71eed463da01e2e6a8c10e6a0
DeltaHelios <stefan.v.iancu@gmail.com>
2025-01-25 23:18:50 +0200

    Datagen

--- src/client/java/com/proxtextchat/ProxChatBaseModDataGenerator.java
+++ src/client/java/com/proxtextchat/ProxChatBaseModDataGenerator.java
@@ -2,10 +2,35 @@ package com.proxtextchat;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
 
 public class ProxChatBaseModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		var pack = fabricDataGenerator.createPack();
+		pack.addProvider(ItemTagGenerator::new);
 	}
+	private static class ItemTagGenerator extends FabricTagProvider.ItemTagProvider
+	{
+		public ItemTagGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture)
+		{
+			super(output, completableFuture);
+		}
+
+		@Override
+		protected void configure(RegistryWrapper.WrapperLookup wrapperLookup)
+		{
+			getOrCreateTagBuilder(ProxChatBaseMod.SEND_IN_HAND);
+			getOrCreateTagBuilder(ProxChatBaseMod.SEND_IN_HOTBAR);
+			getOrCreateTagBuilder(ProxChatBaseMod.SEND_IN_INVETORY);
+			getOrCreateTagBuilder(ProxChatBaseMod.RECEIVE_IN_HAND);
+			getOrCreateTagBuilder(ProxChatBaseMod.RECEIVE_IN_HOTBAR);
+			getOrCreateTagBuilder(ProxChatBaseMod.RECEIVE_IN_INVETORY);
+		}
+	}
+
 }
