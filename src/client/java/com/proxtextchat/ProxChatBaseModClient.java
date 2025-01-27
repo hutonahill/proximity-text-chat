11d898717894e062bf6d0e3b0d8af76169902671
hutonahill <Delaware.white@gmail.com>
2025-01-27 09:13:12 -0700

    removed a file that was left in on accident.

--- src/client/java/com/proxtextchat/ProxChatBaseModClient.java
+++ src/client/java/com/proxtextchat/ProxChatBaseModClient.java
@@ -1,10 +1,27 @@
 package com.proxtextchat;
 
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.text.Text;
 
+/**
+ * main class for the client.
+ */
 public class ProxChatBaseModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ConfigBuilder builder = ConfigBuilder.create()
+				.setParentScreen(/*need to figure out how to get the partent screen*/)
+				.setTitle(Text.literal("Proximity Chat Base Mod Configuration"));
+
+		builder.setSavingRunnable(() -> {
+			// This should be code to read the config the used made and modify the game rules/server config.
+			// This means we can safely disable the ClothConfig without losing functionality.
+		});
+
+		ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+
+
 	}
 }
